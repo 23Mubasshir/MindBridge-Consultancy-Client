a478f377d4cbeb139b500ec8fb1c81719590daeb
23Mubasshir <23mubasshir@gmail.com>
2023-08-04 00:21:32 +0600

    Footer Done

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,7 +1,53 @@
 const Footer = () => {
   return (
     <div>
-      
+      <footer className="footer p-10 bg-black text-white px-96">
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+        <div>
+          <span className="footer-title">Join Our Newsletter</span>
+          <div className="form-control w-80">
+            <label className="label">
+              <span className="label-text text-white">
+                Enter your email address
+              </span>
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="username@site.com"
+                className="input w-full pr-16 rounded-none"
+              />
+              <button className="btn bg-red-700 border-red-900 rounded-none text-white absolute top-0 right-0 hover:text-black">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="footer footer-center p-4 bg-red-700 text-white">
+        <div>
+          <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+        </div>
+      </footer>
     </div>
   );
 };
