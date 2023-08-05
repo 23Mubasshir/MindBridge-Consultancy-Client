c1e01a9b0deaacd8896d8ea8763b6e5709342260
23Mubasshir <23mubasshir@gmail.com>
2023-08-05 11:24:48 +0600

    session section updated

--- src/Pages/Home/Sections/OnetoOneSession/OnetoOneSession.jsx
+++ src/Pages/Home/Sections/OnetoOneSession/OnetoOneSession.jsx
@@ -18,9 +18,16 @@ const OneToOneSession = () => {
               </h1>
             </div>
             <div className="">
-                <p className="text-[20px]">Upgrade Your Business</p>
-                <h1 className="text-5xl font-semibold py-4" >Let&apos;s have a Free one on one session</h1>
-                <p className="text-[20px]" >1-2-1 Consultancy sessions can be booked either as individual sessions, or alternatively we are able to offer packages of multiple sessions, payable in advance. But for the first time it is free for once.</p>
+              <p className="text-[20px]">Upgrade Your Business</p>
+              <h1 className="text-5xl font-semibold py-4">
+                Let&apos;s have a Free one on one session
+              </h1>
+              <p className="text-[20px]">
+                1-2-1 Consultancy sessions can be booked either as individual
+                sessions, or alternatively we are able to offer packages of
+                multiple sessions, payable in advance. But for the first time it
+                is free for once.
+              </p>
             </div>
           </div>
 
@@ -28,6 +35,12 @@ const OneToOneSession = () => {
             <img src={img} className="h-[500px] object-cover" alt="" />
           </div>
         </div>
+      </div>
+      <div className="bg-[---primary-color] flex py-20 justify-center items-center">
+        <h1 className="text-2xl px-12 md:text-4xl lg:text-5xl text-white md:pe-10 lg:pe-72">
+          Let&apos;s have a Free one on one session.
+        </h1>
+        <button className="button-outline me-12">Book Now</button>
       </div>
     </div>
   );
